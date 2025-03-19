688d6b748512f12d8a9f50a6b98d5cbdce6cddde
a71143055 <a71143055@gmail.com>
2025-03-19 11:43:19 +0900

    BookMarket

---
+++ src/main/java/kr/ac/kopo/jeong/bookmarket/service/BookService.java
@@ -0,0 +1,9 @@
+package kr.ac.kopo.jeong.bookmarket.service;
+
+import kr.ac.kopo.jeong.bookmarket.domain.Book;
+
+import java.util.List;
+
+public interface BookService {
+    List<Book> getAllBookList();
+}
---
+++ src/main/java/kr/ac/kopo/jeong/bookmarket/service/BookServiceImpl.java
@@ -0,0 +1,20 @@
+package kr.ac.kopo.jeong.bookmarket.service;
+
+import kr.ac.kopo.jeong.bookmarket.domain.Book;
+import kr.ac.kopo.jeong.bookmarket.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public List<Book> getAllBookList() {
+        return bookRepository.getAllBookList();
+    }
+}
