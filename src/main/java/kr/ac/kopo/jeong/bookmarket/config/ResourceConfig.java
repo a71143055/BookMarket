8b95388511571f8db679401ba0c773a71697a86c
a71143055 <a71143055@gmail.com>
2025-05-07 11:25:40 +0900

    BookMarket

--- src/main/java/kr/ac/kopo/jeong/bookmarket/config/ResourceConfig.java
+++ src/main/java/kr/ac/kopo/jeong/bookmarket/config/ResourceConfig.java
@@ -3,6 +3,7 @@ package kr.ac.kopo.jeong.bookmarket.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ResourceConfig {
