c14832e399fb66f045aeadc0585333608fa0b70e
a71143055 <a71143055@gmail.com>
2025-05-14 10:50:13 +0900

    BookMarket

--- src/main/java/kr/ac/kopo/jeong/bookmarket/domain/Book.java
+++ src/main/java/kr/ac/kopo/jeong/bookmarket/domain/Book.java
@@ -1,5 +1,6 @@
 package kr.ac.kopo.jeong.bookmarket.domain;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,8 +8,13 @@ import java.math.BigDecimal;
 
 @Data
 public class Book {
+    @Pattern(regexp = "isbn[0-9]+")
     private String bookId;
+    @Size(min = 4, max = 50)
     private String name;
+    @Min(0)
+    @Digits(integer = 8, fraction = 2)
+    @NotNull
     private BigDecimal unitPrice;
     private String author;
     private String description;
