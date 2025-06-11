9426e3666e51e518a787bcb3aadb0a667c98fd50
a71143055 <a71143055@gmail.com>
2025-06-11 10:34:58 +0900

    BookMarket

--- src/main/java/com/springboot/service/CartServiceImpl.java
+++ src/main/java/com/springboot/service/CartServiceImpl.java
@@ -1,5 +1,18 @@
 package com.springboot.service;
 
-public class CartServiceImpl {
+import com.springboot.domain.Cart;
+import com.springboot.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class CartServiceImpl {
+    @Autowired
+    private CartRepository cartRepository;
+    public Cart create(Cart cart) {
+        return cartRepository.create(cart);
+    }
+    public Cart read(String cartId) {
+        return cartRepository.read(cartId);
+    }
 }
