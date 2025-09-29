b2792fcc17c2ffb1ee2554901deb14fe4dcb32bf
a71143055 <a71143055@gmail.com>
2025-09-29 09:38:26 +0900

    BookMarket

---
+++ src/main/java/com/springboot/repository/OrderRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.springboot.repository;
+
+import com.springboot.domain.Order;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class OrderRepositoryImpl implements OrderRepository {
+    private Map<Long, Order> listOfOrders;
+    private long nextOrderId;
+    public OrderRepositoryImpl() {
+        listOfOrders = new HashMap<Long, Order>();
+        nextOrderId = 2000;
+    }
+    public Long saveOrder(Order order) {
+        order.setOrderId(getNextOrderId());
+        listOfOrders.put(order.getOrderId(), order);
+        return order.getOrderId();
+    }
+    private synchronized Long getNextOrderId() {
+        return nextOrderId++;
+    }
+}
